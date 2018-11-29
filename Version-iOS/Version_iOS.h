7aefc4e75cb93bd8cb9fe4a0f20391ba89178127
Sourav Chandra <saurav.chandra1992@live.com>
2018-11-29 16:29:25 +0530

    Added ios target

---
+++ Version-iOS/Version_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Version_iOS.h
+//  Version-iOS
+//
+//  Created by Sourav Chandra on 29/11/18.
+//  Copyright © 2018 Marius Rackwitz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Version_iOS.
+FOUNDATION_EXPORT double Version_iOSVersionNumber;
+
+//! Project version string for Version_iOS.
+FOUNDATION_EXPORT const unsigned char Version_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Version_iOS/PublicHeader.h>
+
+
